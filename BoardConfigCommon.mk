28032824dd4d65ba8aebbd947a602f6e50c40395
Martin Liu <liumartin@google.com>
2024-07-07 16:47:44 +0000

    sm6225-common: Disable zone dma32 & Remove video entry from cmdline
    As HW doesn't have 32 bit address limitation, we can disable
    zone dma32 to simplify memory management and improve performance.
    
    Bug: 224535283
    Test: boot, test basic wifi, faceauth, camera, UDFP features
    Change-Id: I9c61e3b7a798c29c68ec534949b76f3ef648f1e6
    Co-Authored-By: Jake Weinstein <jake@aospa.co>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -96,7 +96,7 @@ BOARD_BOOTCONFIG := \
     androidboot.usbcontroller=4e00000.dwc3
 
 BOARD_KERNEL_CMDLINE := \
-    video=vfb:640x400,bpp=32,memsize=3072000
+    disable_dma32=on
 
 TARGET_KERNEL_ARCH := arm64
 INLINE_KERNEL_BUILDING := true
