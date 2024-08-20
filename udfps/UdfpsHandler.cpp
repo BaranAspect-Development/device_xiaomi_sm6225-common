dd50a2ce7c11db2eb9876300cd3b558839cefbd8
Luman Rahman <flyluman@gmail.com>
2024-08-20 09:51:01 +0600

    Livedisplay: Change xiaomi_sm6225 to sm6225

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -4,7 +4,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-#define LOG_TAG "UdfpsHandler.xiaomi_sm6225"
+#define LOG_TAG "UdfpsHandler.sm6225"
 
 #include <android-base/logging.h>
 #include <android-base/properties.h>
