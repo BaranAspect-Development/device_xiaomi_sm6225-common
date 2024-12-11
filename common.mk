54aaf9b3b6a412cfae0c66671e99a1f4de988bb3
Jabiyeff <cebiyevanar@gmail.com>
2024-12-11 18:30:38 +0000

    sm6225-common: Remove duplicate bootctrl namespace
    * Its already defined on Android.bp
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- common.mk
+++ common.mk
@@ -553,7 +553,6 @@ PRODUCT_SHIPPING_API_LEVEL := $(BOARD_SHIPPING_API_LEVEL)
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/qcom-caf/bootctrl \
     hardware/xiaomi
 
 # Telephony
